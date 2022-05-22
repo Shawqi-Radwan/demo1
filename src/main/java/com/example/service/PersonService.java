0a1c2ad48a130f537ad7690c78712a0646766ece
Shawqi-Radwan <104564717+Shawqi-Radwan@users.noreply.github.com>
2022-05-22 18:53:59 +0300

    add new person for test web hook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(22);
+		p.setFirstName("Shawqi");
+		p.setLastName("Radwan");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
